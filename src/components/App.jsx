6e2ca0710c8e715f739383588af883f2e9e9c25c
Hatori <ahgangadm@gmail.com>
2024-04-05 22:30:27 +0300

    Create main App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Description from "./Description/Description";
+import Options from "./Options/Options";
+import Feedback from "./Feedback/Feedback";
+import { useState, useEffect } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+import style from "./App.module.css";
+
+const App = () => {
+  const [data, setData] = useState(() => {
+    const checkData = localStorage.getItem("reviews");
+    if (checkData !== null) {
+      return JSON.parse(checkData);
+    }
+    return { good: 0, neutral: 0, bad: 0 };
+  });
+  useEffect(() => {
+    localStorage.setItem("reviews", JSON.stringify(data));
+  }, [data]);
+
+  // Dont need to create 3 independent functions, rather create 1 function with parameter
+  function handleSetReviews(type) {
+    setData({
+      ...data,
+      [type]: data[type] + 1,
+    });
+  }
+
+  function handleResetReviews() {
+    setData({
+      ...data,
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  }
+
+  const reviewTotal = data.good + data.bad + data.neutral;
+  const percentCount = Math.round((data.good / reviewTotal) * 100);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Description />
+      <Options
+        onFunc={handleSetReviews}
+        review={reviewTotal}
+        onRes={handleResetReviews}
+      />
+      {reviewTotal !== 0 ? (
+        <Feedback object={data} total={reviewTotal} percent={percentCount} />
+      ) : (
+        <p className={style.message}>Not feedback yet</p>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
